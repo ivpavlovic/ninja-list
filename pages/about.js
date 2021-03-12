390f894c69c03836dbdf945eda551d52500d58f9
Marco <marko.pav@gmail.com>
2021-03-12 15:17:17 +0100

    initial

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const About = () => {
+    return ( 
+        <>
+            <Head>
+             <title>Ninja List | About</title> 
+             <meta name="keywords" content="ninjas" />  
+            </Head>
+        <div>
+            <h1>About</h1>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p> 
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p> 
+        </div>
+        </>
+     );
+}
+ 
+export default About;
